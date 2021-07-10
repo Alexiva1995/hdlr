8439f265695593cb4a88fac9f3d31ee6932d8c67
luis briceño <siulbriceno20@gmail.com>
2021-07-10 19:48:18 -0400

    cambiado p/l por ganancia

--- resources/views/inversiones/index.blade.php
+++ resources/views/inversiones/index.blade.php
@@ -15,11 +15,11 @@
                                     <th>Correo</th>
                                     <th>Paquete</th>
                                     <th>Ahorro</th>
-                                    <th>Ganancia</th>
+                                    <th>P/L</th>
                                     <th>Capital</th>
-                                    <th>% Ganancia</th>
-                                    <th>Ganancia acumulada</th>
-                                    <th>Porcentaje fondo</th>
+                                    <th>% P/L</th>
+                                    <th>P/L acumulado</th>
+                                    {{--<th>Porcentaje fondo</th>--}}
                                     <th>Fecha Vencimiento</th>
                                 </tr>
 
@@ -41,7 +41,7 @@
                                     <td>$ {{number_format($inversion->capital, 2, ',', '.')}}</td>
                                     <td>{{number_format($porcentaje,2, ',', '.')}} %</td>
                                     <td>$ {{number_format($inversion->ganancia_acumulada,2, ',', '.')}}</td>
-                                    <td>{{number_format($inversion->porcentaje_fondo,2, ',', '.')}} %</td>
+                                    {{--<td>{{number_format($inversion->porcentaje_fondo,2, ',', '.')}} %</td>--}}
                                     <td>{{date('Y-M-d', strtotime($inversion->fecha_vencimiento))}}</td>
                                 </tr>
                                 @endforeach
